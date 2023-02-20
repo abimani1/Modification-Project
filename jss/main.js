2ae33c22a7af8cc65112fa3af37592e086fa3da8
tharani3920 <122532168+tharani3920@users.noreply.github.com>
2023-02-20 18:17:05 +0530

    praveen and tharani changes

---
+++ jss/main.js
@@ -0,0 +1,51 @@
+/**
+   * Testimonials Slider
+   */
+new Swiper('.testimonials-slider', {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  }
+});
+
+/**
+ * Testimonials Slider
+ */
+new Swiper('.portfolio-details-slider', {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  }
+});
+
+/**
+ * Animation on scroll function and init
+ */
+function aos_init() {
+  AOS.init({
+    duration: 1000,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false
+  });
+}
+window.addEventListener('load', () => {
+  aos_init();
+});
+
